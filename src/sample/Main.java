1cbee5b8b993a2747edf4aa63d4cfc240a4cdfe7
Mantas_MSI <mantasling@gmail.com>
2017-09-18 10:47:51 +0200

    SecondCommit_SetUpisDone

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
+        primaryStage.setTitle("Kino XP");
+        primaryStage.setScene(new Scene(root, 800, 700));
         primaryStage.show();
     }
 
