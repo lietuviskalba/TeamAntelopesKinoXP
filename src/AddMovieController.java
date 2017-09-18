882f2280b8da6802997e5b1c4d8a5b132c95863c
aSeddik93 <seddikabdou@gmail.com>
2017-09-18 11:22:06 +0200

    Abdallah commit 2

--- src/AddMovieController.java
+++ src/AddMovieController.java
@@ -7,11 +7,6 @@ public class AddMovieController {
 
     public void addMovieButton(ActionEvent event){
         if(event.getSource().equals(addMovieButton);{
-            Motorhome byeMotorhome = motorhomesTable.getSelectionModel().getSelectedItem();
-            if(!db.deleteMotorhome(fleet,byeMotorhome))
-                System.out.println("Could not delete Motorhome right now.");
-            else
-                System.out.println("Motorhome deleted from records.");
 
         }
     }
